d845bb413af71c7ba5ac0fc37c2ea0e7e5a4d4ea
Hasse R. Hansen <hasse@ramlev.dk>
2011-08-25 17:16:04 +0200

    Code cleanup.

--- mkdru_ding.js
+++ mkdru_ding.js
@@ -8,16 +8,14 @@ Drupal.theme.prototype.mkdruFacetContainer = function (facetsCfg) {
     facetsCfg[fname].originalKey = fname;
     fs.push(facetsCfg[fname]);
   }
-  fs.sort(function (a,b) { return a.orderWeight - b.orderWeight });
+  fs.sort(function (a,b) { return a.orderWeight - b.orderWeight; });
   var html = '<h2>Facet browser</h2><div class="content">';
   for (var i=0; i<fs.length; i++) {
     var f = fs[i];
     //not display
-    html += '<div id="mkdru-container-'+f.originalKey
-      +'" style="display: none;">';
-    html += '<fieldset class="form-wrapper">'
-    html += '<legend><span class="fieldset-legend">'+f.displayName
-      +'</span></legend>';
+    html += '<div id="mkdru-container-'+f.originalKey +'" style="display: none;">';
+    html += '<fieldset class="form-wrapper">';
+    html += '<legend><span class="fieldset-legend">'+f.displayName +'</span></legend>';
     html += '<div class="fieldset-wrapper">';
     html += '<div class="mkdru-facet-'+f.originalKey+' form-checkboxes"/>';
     html += '</div>';
