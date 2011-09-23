4ef8cf8e1e2425512d8b93b64b9749b90a8a88dc
Hasse R. Hansen <hasse@ramlev.dk>
2011-09-23 11:47:09 +0200

    Validate code in jslint

--- mkdru.theme.js
+++ mkdru.theme.js
@@ -2,24 +2,24 @@ Drupal.theme.mkdruResult = function(hit, num, detailLink) {
   var link = choose_url(hit);
   if (!link) link = choose_url(hit['location'][0]);
   var html = "";
-  html += '<li class="search-result" id="rec_' + hit.recid + '" >'
-    + '<h3 class="title">';
+  html += '<li class="search-result" id="rec_' + hit.recid + '" >' +
+    '<h3 class="title">';
   if (link) html += '<a href="'+link+'" target="_blank" >';
   html += hit["md-title"];
   if (link) html += '</a>';
   html += '</h3>';
-  html += '<div class="search-snippet-info">'
-     + '<p class="search-snippet"></p>'
-     + '<div class="ting-object clearfix">'
-      + '<div class="ting-overview clearfix">'
-        + '<div class="left-column left">'
-          + '<div class="picture"></div>'
-        + '</div>'
-        + '<div class="right-column left">';
+  html += '<div class="search-snippet-info">' +
+      '<p class="search-snippet"></p>' +
+      '<div class="ting-object clearfix">' +
+       '<div class="ting-overview clearfix">' +
+         '<div class="left-column left">' +
+           '<div class="picture"></div>' +
+         '</div>' +
+         '<div class="right-column left">';
   if (hit["md-author"]) {
-    html += '<div class="creator"><span class="byline">By </span>'
-      + '<a class="author" href="/search/meta/'+hit['md-author']+'">'
-      + hit['md-author']+'</a>';
+    html += '<div class="creator"><span class="byline">By </span>' +
+       '<a class="author" href="/search/meta/'+hit['md-author']+'">' +
+       hit['md-author']+'</a>';
     if (hit['md-date']) {
       html += '<span class="date"> ('+hit['md-date']+')</span>';
     }
@@ -30,9 +30,9 @@ Drupal.theme.mkdruResult = function(hit, num, detailLink) {
     // limit description to 400 characters
     html += hit["md-description"][0].substr(0, 400);
   }
-  html += '</div>'
-  html += '</div>'
-  html += '</div>'
+  html += '</div>';
+  html += '</div>';
+  html += '</div>';
   html += '</li>';
   return html;
 };
@@ -43,16 +43,15 @@ Drupal.theme.mkdruFacetContainer = function (facetsCfg) {
     facetsCfg[fname].originalKey = fname;
     fs.push(facetsCfg[fname]);
   }
-  fs.sort(function (a,b) { return a.orderWeight - b.orderWeight });
+  fs.sort(function (a,b) { return a.orderWeight - b.orderWeight; });
   var html = '<h2>Facet browser</h2><div class="content">';
   for (var i=0; i<fs.length; i++) {
     var f = fs[i];
     //not display
-    html += '<div id="mkdru-container-'+f.originalKey
-      +'" style="display: none;">';
-    html += '<fieldset class="form-wrapper">'
-    html += '<legend><span class="fieldset-legend">'+f.displayName
-      +'</span></legend>';
+    html += '<div id="mkdru-container-'+f.originalKey +
+      '" style="display: none;">';
+    html += '<fieldset class="form-wrapper">';
+    html += '<legend><span class="fieldset-legend">'+f.displayName +'</span></legend>';
     html += '<div class="fieldset-wrapper">';
     html += '<div class="mkdru-facet-'+f.originalKey+' form-checkboxes"/>';
     html += '</div>';
@@ -72,26 +71,23 @@ Drupal.theme.mkdruFacet = function (terms, facet, max, selections) {
     var term = terms[i];
     var id = term.name.split(/w+/).join("-").toLowerCase();
     html += '<div class="form-item form-type-checkbox">';
-    html += '<input type="checkbox" id="'+id+'" onclick="window.location=\''
-      +term.toggleLink+'\'; return true;" class="form-checkbox"';
+    html += '<input type="checkbox" id="'+id+'" onclick="window.location=\'' +term.toggleLink+'\'; return true;" class="form-checkbox"';
     if (term.selected) html += ' checked="checked"';
     html += '/><label class="option" for="'+id+'">'+term.name; 
     html += '<span> ('+term.freq+')</span></label></div>';
   }
-  if (terms.length == 0 && selections && selections.length) {
+  if (terms.length === 0 && selections && selections.length) {
     for (var i=0; i<selections.length; i++) {
       show = true;
       if (selections[i]) {
         // since we have no target name (only id) go for the basename
         // FIXME get the proper target name
-        var name = facet == "source" ? selections[i].replace(/.*[\/\\]/, "")
-          .replace(/\?.*/, '')
-          : selections[i];
+        var name = facet == "source" ? selections[i].replace(/.*[\/\\]/, "").replace(/\?.*/, '') : selections[i];
         html += '<div class="form-item form-type-checkbox">';
-        html += '<input type="checkbox" checked="checked" id="'+name+'" '
-          +'onclick="window.location=\''+mkdru.removeLimit(facet, selections[i])
-          +'\';return true;" class="form-checkbox"/><label class="option" for="'
-          +name+'">'+name+'</a><span> (0)</span></label></div>';
+        html += '<input type="checkbox" checked="checked" id="'+name+'" ' +
+          'onclick="window.location=\''+mkdru.removeLimit(facet, selections[i]) +
+          '\';return true;" class="form-checkbox"/><label class="option" for="' +
+          name+'">'+name+'</a><span> (0)</span></label></div>';
       }
     }
   }
--- recipe.js
+++ recipe.js
@@ -1,14 +1,12 @@
 
 function choose_url (data, proxyPattern) {
     //first try to prepare local_url from recipe
-    var local_url = data["md-url_recipe"] !== undefined
-        ? prepare_url(data["md-url_recipe"][0], data) : null;
+    var local_url = data["md-url_recipe"] !== undefined ? prepare_url(data["md-url_recipe"][0], data) : null;
 
-    var use_url_proxy = data["md-use_url_proxy"] !== undefined
-        ? data["md-use_url_proxy"] : "0";
+    var use_url_proxy = data["md-use_url_proxy"] !== undefined ? data["md-use_url_proxy"] : "0";
 
     //use the proxyPattern
-    if (proxyPattern && use_url_proxy == "1") {        
+    if (proxyPattern && use_url_proxy == "1") {
         if (local_url) {
             data["md-local-url"] = [];
             data["md-local-url"].push(local_url);
@@ -22,13 +20,12 @@ function choose_url (data, proxyPattern) {
         return local_url;
 
     //local failed, go for resource
-    return data["md-electronic-url"] !== undefined
-        ? data["md-electronic-url"][0] : null;
+    return data["md-electronic-url"] !== undefined ? data["md-electronic-url"][0] : null;
 }
 
-var XRef = function (url, text) {  
+var XRef = function (url, text) {
   this.url = url;
-  this.text = text;  
+  this.text = text;
 };
 
 function has_recipe (data) {
@@ -47,7 +44,7 @@ function getUrlFromRecipe (data) {
     return prepare_url(data["md-url_recipe"][0],data);
   } else {
     return null;
-  }  
+  }
 }
 
 function getElectronicUrls (data) {
@@ -56,7 +53,7 @@ function getElectronicUrls (data) {
     for (var i=0; i<data["md-electronic-url"].length; i++) {
       var linkUrl = data["md-electronic-url"][i];
       var linkText = data["md-electronic-text"][i];
-      var ref = new XRef(linkUrl, (linkText.length==0 ? "Web Link" : linkText));
+      var ref = new XRef(linkUrl, (linkText.length === 0 ? "Web Link" : linkText));
       urls.push(ref);
     }
   }
@@ -66,14 +63,16 @@ function getElectronicUrls (data) {
 
 // Prepares urls from recipes with expressions in the form:
 // ${variable-name[pattern/replacement/mode]}, [regex] is optional
-// eg. http://sever.com?title=${md-title[\s+//]} will strip all whitespaces 
+// eg. http://sever.com?title=${md-title[\s+//]} will strip all whitespaces
 function prepare_url(url_recipe, meta_data) {
-    if (typeof url_recipe != "string" || url_recipe.length == 0)
+    if (typeof url_recipe != "string" || url_recipe.length === 0) {
         return null;
-    if (typeof meta_data != "object")
+    }
+    if (typeof meta_data != "object") {
         return null;
+    }
     try {
-        return url_recipe.replace(/\${[^}]*}/g, function (match) { 
+        return url_recipe.replace(/\${[^}]*}/g, function(match) {
           return get_var_value(match, meta_data);
         });
     } catch (e) {
@@ -98,7 +97,7 @@ function get_var_value (expr_in, meta_data) {
 }
 
 // exec perl-like substitution regexes in the form: pattern/replacement/mode
-function exec_sregex (regex_str, input_str) {   
+function exec_sregex (regex_str, input_str) {
     var regex_parts = ["", "", ""];
     var i = 0;
     for (var j=0; j<regex_str.length && i<3; j++) {
@@ -108,7 +107,7 @@ function exec_sregex (regex_str, input_str) {
             regex_parts[i] += regex_str.charAt(j);
     }
     var regex_obj = new RegExp(regex_parts[0], regex_parts[2]);
-    return input_str.replace(regex_obj, regex_parts[1]);    
+    return input_str.replace(regex_obj, regex_parts[1]);
 }
 
 function test_url_recipe() {
